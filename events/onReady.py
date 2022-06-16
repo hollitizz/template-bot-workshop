f7a0be5a1a0440f2d3e18cc637de0a9fb369fe1d
Valentin <30577852+hollitizz@users.noreply.github.com>
2022-06-16 19:38:29 +0200

    Update onReady.py

--- events/onReady.py
+++ events/onReady.py
@@ -2,13 +2,12 @@ import discord
 
 from utils.types import Setup
 
-
 async def onReady(self: Setup):
     await self.change_presence(
         status=discord.Status.online,
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="League of Legends"
+            name="A discord.py Workshop"
         )
     )
     print(f"{self.user} is Ready !")
